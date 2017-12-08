7797425ebb5f0753823986b92953fe6e2f6cd943
Auguste Dutcher <aug@Augustes-MacBook-Pro.local>
2017-12-07 20:34:43 -0800

    removed duplicate indexing

--- parse_cmsearch.py
+++ parse_cmsearch.py
@@ -42,75 +42,85 @@ cmsearch = ['target_name', 'target_accession', 'query_name', 'query_accession', 
 with open('cm_dict.p', 'rb') as infile:
     cm_dict = pickle.load(infile)
 
-# Make a master dataframe that contains all the top hits
-rdf = pd.DataFrame()
-for file in os.listdir('.'):
-    if file.endswith('.txt'):
-        df = pd.read_csv(file, delim_whitespace=True, header=None, names=cmsearch, \
-            skiprows=2, skipfooter=10, engine='python', index_col=False, usecols=cmsearch)
+# Create Excel workbook to write to
+with open('rfam_results.xlsx', 'ab') as outfile:
+    writer = pd.ExcelWriter(outfile)
 
-        # Fix the sRNA names if necessary
-        spl = lambda x: x.split('_seqs_')[0]
-        if file.startswith('RF'):
-            df['srna_name'] = df['query_accession'].map(cm_dict)
-        else:
-            df['srna_name'] = df.query_name.apply(spl)
-        
-        print(df)
+    # Make a master dataframe that contains all the top hits
+    rdf = pd.DataFrame()
+    for file in os.listdir('.'):
+        if file.endswith('.txt'):
+            df = pd.read_csv(file, delim_whitespace=True, header=None, names=cmsearch, \
+                skiprows=2, skipfooter=10, engine='python', index_col=False, usecols=cmsearch)
 
-        # Get the top hit only
-        fil = df.groupby('target_name')['E-value'].nsmallest(1)
+            # Fix the sRNA names if necessary
+            spl = lambda x: x.split('_seqs_')[0]
+            if file.startswith('RF'):
+                df['srna_name'] = df['query_accession'].map(cm_dict)
+            else:
+                df['srna_name'] = df.query_name.apply(spl)
+            
+            print(df)
 
-        # Account for weird indexing caused by the groupby operation
-        if fil.index.dtype != 'int64':
-            fil.index = fil.index.droplevel(level=0)
-        keep = df.loc[fil.index]
-        print(keep)
-        rdf = rdf.append(keep)
-        print(rdf)
+            # Get the top hit only
+            fil = df.groupby('target_name')['E-value'].nsmallest(1)
 
-print(rdf)
+            # Account for weird indexing caused by the groupby operation
+            if fil.index.dtype != 'int64':
+                fil.index = fil.index.droplevel(level=0)
+            keep = df.loc[fil.index]
+            print(keep)
+            rdf = rdf.append(keep)
+            print(rdf)
 
-# Reset the index to cope with any duplicate indices
-rdf = rdf.reset_index()
+    print(rdf)
 
-# Add columns to reflect minimum and maximum as start/end
-rdf['start'] = rdf[['seq_from','seq_to']].min(axis=1)
-rdf['end'] = rdf[['seq_from','seq_to']].max(axis=1)
+    # Reset the index to cope with any duplicate indices
+    rdf = rdf.reset_index()
 
-# Add column to show sRNA name
+    # Add columns to reflect minimum and maximum as start/end
+    rdf['start'] = rdf[['seq_from','seq_to']].min(axis=1)
+    rdf['end'] = rdf[['seq_from','seq_to']].max(axis=1)
 
+    print(rdf)
+    # Remove results that don't cover at least 60% original query length
+    with open('srna_len.p', 'rb') as infile:
+        srna_len = pickle.load(infile)
 
-print(rdf)
-# Remove results that don't cover at least 60% original query length
-with open('srna_len.p', 'rb') as infile:
-    srna_len = pickle.load(infile)
+    rdf['srna_orig_len'] = rdf['srna_name'].map(srna_len)
 
-rdf['srna_orig_len'] = rdf['srna_name'].map(srna_len)
+    # Calculate the target length
+    rdf['targ_len'] = rdf['end'].astype(int) - rdf['start'].astype(int) + 1
+    rdf['perc_coverage'] = rdf['targ_len']/rdf['srna_orig_len']
 
-# Calculate the target length
-rdf['targ_len'] = rdf['end'].astype(int) - rdf['start'].astype(int) + 1
-rdf['perc_coverage'] = rdf['targ_len']/rdf['srna_orig_len']
+    rdf = rdf.drop(rdf[rdf.perc_coverage < 0.60].index)
 
-rdf = rdf.drop(rdf[rdf.perc_coverage < 0.60].index)
+    # Identify and remove overlaps
+    rdf_nr = rdf.groupby(['target_name', 'strand'], as_index=False).apply(compare_rows)
 
-# Identify and remove overlaps
-rdf_nr = rdf.groupby(['target_name', 'strand'], as_index=False).apply(compare_rows)
+    # Drop column named 'index' (not the real index)
+    rdf_nr.drop(['index'], axis=1)
 
-# Drop the extra level of indexing gained via the groupby operation
-#rdf_nr.index = rdf_nr.index.droplevel(level=0)
+    # Drop the extra level of indexing gained via the groupby operation
+    rdf_nr.index = rdf_nr.index.droplevel(level=0)
 
-# Make and save a presence dictionary
-# Create an empty dictionary to hold the genomes for which each sRNA is present
-present_dict = {}
+    # Find the difference between the original results and the nr results, to be saved and examined later
+    overlaps = rdf[~rdf.index.isin(rdf_nr.index)]
 
-srna_grp = rdf_nr.groupby(rdf_nr['srna_name'])
+    rdf_nr.to_excel(writer, sheet_name='nr_cmsearch')
+    overlaps.to_excel(writer, sheet_name='removed_due_to_overlap')
+    writer.save()
 
-for name, data in srna_grp:
-    print(name)
-    present = data['target_name'].tolist()
-    present_dict[name] = set(acc for acc in present)
+    # Create an empty dictionary to hold the genomes for which each sRNA is present
+    present_dict = {}
 
-# Write presence dictionary to file
-with open('rfam_presence_dict.p', 'wb') as outfile:
-    pickle.dump(present_dict, outfile)
+    srna_grp = rdf_nr.groupby(rdf_nr['srna_name'])
+
+    for name, data in srna_grp:
+        print(name)
+        present = data['target_name'].tolist()
+        present_dict[name] = set(acc for acc in present)
+
+    # Write presence dictionary to file
+    with open('rfam_presence_dict.p', 'wb') as outfile:
+        pickle.dump(present_dict, outfile)
