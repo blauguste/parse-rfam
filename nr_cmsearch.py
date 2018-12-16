cfe34395322c21f7ddd69e5a7d7b10697756c84d
Auguste Dutcher <aug@Augustes-MacBook-Pro.local>
2018-12-16 12:23:31 -0600

    reflects version running on server

--- nr_cmsearch.py
+++ nr_cmsearch.py
@@ -4,6 +4,13 @@ import sys
 import os
 import numpy as np
 
+def get_basename(str_in):
+    if '_v' in str_in:
+        orig = str_in.split('_v')[0]
+        return orig
+    else:
+        return str_in
+
 def get_acc_num(str_in):
     acc_num = str_in.split('|')[1]
     return acc_num
@@ -24,8 +31,9 @@ def overlap(start1, end1, start2, end2):
     """
     return end1 >= start2 and end2 >= start1
 
-def compare_rows(group):
-    group.sort_values(by='E-value', ascending=True, inplace=True)
+def compare_rows(passed_group):
+    global redun
+    group = passed_group.sort_values(by='E-value', ascending=True)
     print(group)
     winners = []
     skip = []
@@ -42,6 +50,8 @@ def compare_rows(group):
             jend = group.loc[j, 'end']
             if overlap(istart, iend, jstart, jend):
                 winner = group.loc[[i, j], 'E-value'].idxmin()
+                loser = group.loc[[i, j], 'E-value'].idxmax()
+                redun.append((group.loc[winner].query_accession, group.loc[loser].query_accession))
                 if winner == j:
                     winners.append(winner)
                     skip.append(i)
@@ -51,6 +61,9 @@ def compare_rows(group):
     return group.loc[winners].drop_duplicates()
 
 def nr_cmsearch(outfile):
+
+    global redun
+    redun = []
 
     cmsearch = ['target_name', 'target_accession', 'query_name', 'query_accession', \
             'mdl', 'mdl_from', 'mdl_to', 'seq_from', 'seq_to', 'strand', 'trunc', \
@@ -62,7 +75,7 @@ def nr_cmsearch(outfile):
         rdf = pd.DataFrame()
 
         for fn in os.listdir('.'):
-            if fn.endswith('.txt'):
+            if fn.endswith(('.tab', '.txt')):
                 df = pd.read_csv(fn, delim_whitespace=True, header=None, names=cmsearch, \
                     skiprows=2, skipfooter=10, engine='python', index_col=False)
 
@@ -93,8 +106,9 @@ def nr_cmsearch(outfile):
         # Put identifiers all in same column
         rdf = rdf.replace({'query_accession': {'-': np.nan}})
         rdf.query_accession = rdf.query_accession.fillna(rdf.query_name)
+        rdf['basename'] = rdf['query_accession'].apply(get_basename)
 
-        rdf['srna_orig_len'] = rdf['query_accession'].map(srna_len)
+        rdf['srna_orig_len'] = rdf['basename'].map(srna_len)
         rdf['targ_len'] = rdf['end'].astype(int) - rdf['start'].astype(int) + 1
         rdf['perc_coverage'] = rdf['targ_len']/rdf['srna_orig_len']
 
@@ -109,10 +123,12 @@ def nr_cmsearch(outfile):
 
         # Save the nonredundant results for later use
         rdf_nr.to_pickle('nr_cmsearch_results.p')
+        
+        with open('redundancy_tuples.p', 'wb') as outfile:
+            pickle.dump(redun, outfile)
 
         # Find the difference between the original results and the nr results
         overlaps = rdf[~rdf.index.isin(rdf_nr.index)]
-        overlaps.to_csv('test_overlaps.csv')
 
         rdf_nr.to_excel(writer, sheet_name='nr_cmsearch')
         overlaps.to_excel(writer, sheet_name='removed_due_to_overlap')
